61b6cbcda2c7c2e7def12c7cb24728cde7a127e0
Luk Weyens <lukweyens@gmail.com>
2024-12-08 16:04:52 +0100

    day7

---
+++ advent-of-code/day7/part2.py
@@ -0,0 +1,47 @@
+from itertools import product
+
+def get_sum_of_correct_equations(input_file):
+    equations = read_equations_from_file(input_file)
+    sum = 0
+    for equation in equations:
+        if equation.can_be_solved(): sum += equation.result
+
+    return sum
+
+def read_equations_from_file(input_filename):
+    equations = []
+    with open(input_filename, "r") as file:
+        lines = file.readlines()
+    
+    for line in lines:
+        split = line.split(": ")
+        equations.append(Equation(int(split[0]), [int(member) for member in split[1].split(" ")]))
+
+    return equations
+
+class Equation:
+    def __init__(self, result, members):
+        self.result = result
+        self.members = members
+        self.operands = ["+", "*", "||"]
+
+    def can_be_solved(self):
+        operand_permutations = list(product(self.operands, repeat=len(self.members) - 1))
+        for permutation in operand_permutations:
+            result = self.equate(self.members[0], self.members[1], permutation[0])
+            for i in range(1, len(permutation)):
+                result = self.equate(result, self.members[i+1], permutation[i])
+            
+            if result == self.result: return True
+                
+        return False
+    
+    def equate(self, left, right, operand):
+        if operand == "+":
+            return left + right
+        if operand == "*":
+            return left * right
+        if operand == "||":
+            return int(str(left) + str(right))
+        
+        raise Exception(f"{operand} is an unknown operation")
---
+++ advent-of-code/day7/test/test_part2.py
@@ -0,0 +1,26 @@
+import pytest
+import os
+from day7 import part2
+
+def test_sum_of_correct_equations():
+    file_name = write_to_temp_file("""190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20""")
+    sum_of_correct_equations = part2.get_sum_of_correct_equations(file_name)
+    assert sum_of_correct_equations == 11387
+
+def test_full_input():
+    sum_of_correct_equations = part2.get_sum_of_correct_equations('advent-of-code/day7/input.txt')
+    assert sum_of_correct_equations == 264184041398847
+
+def write_to_temp_file(text):
+    file = open("test_input.txt", "w")
+    file.write(text)
+    file.close
+    return os.path.realpath(file.name)
